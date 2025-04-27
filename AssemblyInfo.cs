67ecbdce4b4a2446cae39bb8d8b5762c25d4e08d
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-04-26 21:36:06 -0300

    Actualiza archivos del proyecto y agrega licencia MIT
    Se han añadido entradas al archivo `.gitignore` para ignorar archivos y carpetas específicas, incluyendo configuraciones de IDE y archivos de instalación de Windows.
    
    En `AssemblyInfo.cs`, se han incorporado atributos de ensamblado que describen la biblioteca, incluyendo derechos de autor y visibilidad COM.
    
    Se ha creado un archivo `LICENSE` que detalla la licencia MIT y sus condiciones.
    
    Además, se ha añadido un archivo `SylSoft.Utils.Validators.nuspec` que define los metadatos del paquete NuGet.
    
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

---
+++ AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("A lightweight .NET library for validating common formats and country-specific data.")]
+[assembly: AssemblyCopyright("Copyright © 2025 SylSoft")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("84F69697-4A5E-4FDD-86E6-75F35AA66418")]
