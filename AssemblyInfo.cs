c979c00546b7e988b4037ac9e76c5a549990d48c
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-04-28 15:03:31 -0300

    Actualiza atributos de ensamblado en AssemblyInfo.cs
    Se han eliminado varios atributos de ensamblado en `AssemblyInfo.cs`, como `AssemblyDescription`, `AssemblyCopyright`, `AssemblyTrademark` y `AssemblyCulture`. Se han añadido comentarios que explican la generación automática de estos atributos en proyectos de estilo SDK. Además, se han incorporado nuevos atributos, incluyendo `AssemblyCompany`, `AssemblyConfiguration`, `AssemblyFileVersion`, `AssemblyInformationalVersion`, `AssemblyMetadata`, `AssemblyTitle`, `AssemblyTrademark` y `AssemblyVersion`, junto con un nuevo GUID para el identificador de typelib.
    
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,9 +1,28 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyDescription("A lightweight .NET library for validating common formats and country-specific data.")]
-[assembly: AssemblyCopyright("Copyright © 2025 SylSoft")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+// En proyectos de estilo SDK como este, varios atributos de ensamblado que definían
+// en este archivo se agregan ahora automáticamente durante la compilación y se rellenan
+// con valores definidos en las propiedades del proyecto. Para obtener detalles acerca
+// de los atributos que se incluyen y cómo personalizar este proceso, consulte https://aka.ms/assembly-info-properties
+
+// Al establecer ComVisible en false, se consigue que los tipos de este ensamblado
+// no sean visibles para los componentes COM. Si tiene que acceder a un tipo en este
+// ensamblado desde COM, establezca el atributo ComVisible en true en ese tipo.
+
 [assembly: ComVisible(false)]
-[assembly: Guid("84F69697-4A5E-4FDD-86E6-75F35AA66418")]
+
+// El GUID es para el identificador de typelib, si este proyecto se expone en COM.
+[assembly: AssemblyCompany("Cristian Miguel Zangaro & SylSoft")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright © Cristian Miguel Zangaro 2025 SylSoft")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("A lightweight .NET library for validating common formats and country-specific data.")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0+59364748839e47d42239cac27ff7a115ea11e5c7")]
+[assembly: AssemblyMetadata("ProjectUrl", "https://github.com/Sylsoft-Software-Design/SylSoft.Utils.Validators")]
+[assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/Sylsoft-Software-Design/SylSoft.Utils.Validators")]
+[assembly: AssemblyTitle("SylSoft.Utils.Validators")]
+[assembly: AssemblyTrademark("Copyright © Cristian Miguel Zangaro 2025 SylSoft")]
+[assembly: AssemblyVersion("0.3.*")]
+[assembly: Guid("b02507de-0f0e-4012-a991-f3433c977868")]
